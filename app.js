31ffbaaf28fd223303a5ef1ed4b973f50d66a6b3
miguelgabriel01 <mgbs@discente.ifpe.edu.br>
2021-05-24 15:30:55 -0300

    implementação dos pontos de vida do monstro e do jogador

--- app.js
+++ app.js
@@ -2,13 +2,14 @@ new Vue({
   el: '#app',//elemento pai(div)
   data(){//armazenamos os dados da nossa aplicação
     return{
-
+     playerLife:100,//salvamos nesta variavel o valor que corresponde a vida inicial do jogador
+     monsterLife:100,//salvamos nesta variavel o valor que corresponde a vida inicial do jogador
     }
   },
   methods:{
 
   },
   watch:{
-    
+
   }
 })
