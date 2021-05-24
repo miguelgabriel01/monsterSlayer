9a474aa97353978062955d5916f2af88766adbe2
miguelgabriel01 <mgbs@discente.ifpe.edu.br>
2021-05-24 16:14:01 -0300

    Criação dos botoẽs do jogo

--- app.js
+++ app.js
@@ -2,12 +2,19 @@ new Vue({
   el: '#app',//elemento pai(div)
   data(){//armazenamos os dados da nossa aplicação
     return{
-     playerLife:100,//salvamos nesta variavel o valor que corresponde a vida inicial do jogador
-     monsterLife:100,//salvamos nesta variavel o valor que corresponde a vida inicial do jogador
+      running:true,//atributo que informa se o jogo esta iniciado
+     playerLife:0,//salvamos nesta variavel o valor que corresponde a vida inicial do jogador
+     monsterLife:10,//salvamos nesta variavel o valor que corresponde a vida inicial do jogador
     }
   },
+  computed:{
+   // função que valida se existe um resultado
+   //basicamente ela retorna uma condição como verdadeira caso o jogador ou monsro morreu
+   hasResult(){
+    return this.playerLife == 0 || this.monsterLife == 0;
+  }
+  },
   methods:{
-
   },
   watch:{
 
